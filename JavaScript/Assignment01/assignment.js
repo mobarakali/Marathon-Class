d7cdb331006590b2ea26d548194e0bd59cae1adc
Mobarak Ali <mobarak.ali@gmail.com>
2023-10-25 18:57:36 +0600

    Reivison Completed

--- JavaScript/Assignment01/assignment.js
+++ JavaScript/Assignment01/assignment.js
@@ -71,7 +71,7 @@ function isPrime(number, divisor = 2) {
   return isPrime(number, divisor + 1);
 }
 
-let num = 11;
+let num = 19;
 
 if (isPrime(num)) {
   console.log(num + " is a prime number.");
@@ -103,7 +103,7 @@ function isLeapYear(year) {
   }
 }
 
-console.log(isLeapYear(200)); //
+console.log(isLeapYear(2000)); //
 
 /////////////////////////////////////////////////////////////////////////////////
 // Problem 7: Create a function removeDuplicates that takes an array and returns 
@@ -122,7 +122,7 @@ function removeDuplicates(array) {
   return cleanedArray;
 }
 
-const originalArray = [1, 1, 1, 11, 111, 50, 50, 54, 1, 0];
+const originalArray = [1, 1, 1, 11, 111, 50, 50, 54, 1];
 
 const duplicatesRemoved = removeDuplicates(originalArray);
 
@@ -174,36 +174,27 @@ function evenOdd(str) {
 console.log(evenOdd('JavaScript'));
 console.log(evenOdd('Hello'));
 
-/////////////////////////////////////////////////////////////////////////////////
-
-
-
-
+/****************************************************************************/
 
 // => Answer any 4 questions.
 
 // 1. Explain the difference between 'if...else' and 'switch' statements for conditional logic.
 
-
 // 2. What is JavaScript, and what is its primary purpose in web development?
-/** Answer:
-JavaScript is a scripting language developed by Brendan Eich in the year 1995 for Netscape Navigator. JavaScript's primary purpose in web development is to enhance the user experience, add interactivity, and create dynamic, feature-rich web applications that work seamlessly across different browsers. Now, it is a fundamental technology for modern web development. 
- 
- */
+/** Answer: ************************
+JavaScript is a scripting language developed by Brendan Eich in the year 1995 for Netscape Navigator. JavaScript's primary purpose in web development is to enhance the user experience, add interactivity, and create dynamic, feature-rich web applications that work seamlessly across different browsers. Now, it is a fundamental technology for modern web development. */
 
 // 3. Explain the difference between var, let, and const when declaring variables in JavaScript.
-/** Answer: 
+/** Answer: ************************
 In JavaScript, there are three main ways to declare variables: var, let, and const. Each has distinct characteristics:
 var: Variables declared with var are function-scoped, which means they are only accessible within the function in which they are declared. If declared outside any function, they become globally scoped. Variables declared with var are hoisted to the top of their scope, which means they are moved to the top during the compilation phase, making them available before their actual declaration in the code. var variables can be redeclared within the same scope without any error.
 
 let: Variables declared with let are block-scoped, which means they are only accessible within the block, statement, or curly braces in which they are declared. let variables are not hoisted to the top of their scope, and they cannot be redeclared within the same scope.
 
-const: Variables declared with 'const' are also block-scoped and cannot be reassigned once they are assigned a value. However, it's important to note that when you declare a variable using const, it doesn't mean the value itself is immutable. You can't reassign the variable to a further value, but if the value is an object, you can still modify its properties.
-*/
+const: Variables declared with 'const' are also block-scoped and cannot be reassigned once they are assigned a value. However, it's important to note that when you declare a variable using const, it doesn't mean the value itself is immutable. You can't reassign the variable to a further value, but if the value is an object, you can still modify its properties. */
 
 // 4. Explain the concept of "scope" in JavaScript and the difference between global and local scope.
-/**
- Answer:
+/** Answer: ************************
  In JavaScript, "scope" refers to the context in which variables and functions are declared and accessed. It determines where in your code a particular variable or function is visible and can be used. Understanding scope is crucial for writing clean and error-free code. There are two main types of scope in JavaScript: global scope and local scope.
 Global Scope:
 Variables declared outside of any function or block have global scope. They can be accessed from anywhere in your JavaScript code.
@@ -212,17 +203,15 @@ However, global variables should be used with caution, as they can lead to namin
 Local Scope:
 Variables declared inside a function, block, or a specific code construct have local scope. They are only accessible within the specific function or block in which they are defined.
 Local scope helps in encapsulating variables and functions, preventing unintended interactions and providing data isolation.
-Each function creates its own scope, so variables with the same name can exist in different functions without conflicts.
- */
+Each function creates its own scope, so variables with the same name can exist in different functions without conflicts.  */
 
 // 5. What is the difference between "null" and "undefined" in JavaScript?
-
-/** Answer:
+/** Answer: ************************
  "null": In JavaScript, "null" is a value that represents the intentional absence of any object value or the lack of a value. When a variable has the value of "null", it means that no data is stored inside the variable or it is empty.
  
  "undefined": "undefined" in JavaScript means that a variable has been declared but not assigned a value.
  */
-
+/************************************************************************************ */
 // => Assignment Deadline + Marks:
 // 1. Each question value is 5 marks.
 // 2. The deadline to submit your assignment is October 25, 2023, at 8:00 PM.
